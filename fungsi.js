30476f1e33d263c0c6e0daa698bfa7ee3fbb2d82
Yudha <bambangwirayudha5@gmail.com>
2025-01-10 16:11:11 +0800

    menambahkan fitur multiply

--- fungsi.js
+++ fungsi.js
@@ -2,5 +2,8 @@ console.log('fungsi pertama');
 
 console.log('fungsi kedua');
 
-const add = (c, d) => c + d;
-console.log(add(1,35));
+const add = (a, b) => a + b;
+console.log(add(1,5));
+
+const multiply = (a, b) => a * b;
+console.log(multiply(5,10));
