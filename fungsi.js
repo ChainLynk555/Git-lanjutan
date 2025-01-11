71f98f1da34e04a0fbd9332b026626b134fb91c7
Yudha <bambangwirayudha5@gmail.com>
2025-01-11 15:33:52 +0800

    merevisi commit multiply

--- fungsi.js
+++ fungsi.js
@@ -5,8 +5,8 @@ console.log('fungsi kedua');
 const add = (a, b) => a + b;
 console.log(add(1,5));
 
-const multiply = (a, b) => a * b;
-console.log(multiply(5,10));
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(10,10));
 
 const divide = (a, b) => a / b;
 console.log(divide(50,10));
